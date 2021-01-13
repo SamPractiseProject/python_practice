0e1bac775f082bdb513d2ed0d3cde633e9fd11ce
楊寧 <yyaoning@gmail.com>
2021-01-13 19:36:43 +0800

    debug

--- Lab5/list.py
+++ Lab5/list.py
@@ -1,58 +1,60 @@
 
 #append
-"""x = [1,2,3]
+x = [1,2,3]
 y = [4,5,6]
 x.append(y)
-print(x)"""
-
+print(x)
+print('\n')
 
 #extend
-'''x = [1,2,3]
+x = [1,2,3]
 y = [4,5,6]
 x.extend(y)
-print(x)'''
-
+print(x)
+print('\n')
 
 #insert
-'''x=[1,2,3]
+x=[1,2,3]
 x.insert(2,"aaa")
-print(x)'''
-
+print(x)
+print('\n')
 
 #del
-'''x=[1,2,3,4]
+x=[1,2,3]
 del x[0]
 print(x)
-del x[:3]
+del x[:2]
 print(x)
-'''
+print('\n')
 
 
 #remove(找到第一個和指定內容相同)
-'''x=[1,2,3,4,5,3]
+x=[1,2,3,4,5,3]
 x.remove(3)
 print(x)
 x.remove(3)
-print(x)'''
-
+print(x)
+print('\n')
 
 #sorted
-'''x=[4,2,3,1]
+x=[4,2,3,1]
 y=sorted(x)
 print(y)
 z=sorted(x,reverse=True)
-print(z)'''
-
+print(z)
+print('\n')
 #else list操作
-'''print(3 in [1,2,3])
+print(3 in [1,2,3])
 print(3 not in [1,2,3])
 z=[1,3] * 2
 print(z)
+print('\n')
 a = min([1,2,3,4,6,8])
 b = max([1,2,1,4,5,6])
 print(a)
 print(b)
+print('\n')
 #index找3的位置
 x = [1,2,3,3,3,3,4,5,6]
 print(x.index(3))
-print(x.count(3))'''
+print(x.count(3))
--- Lab5/tuple.py
+++ Lab5/tuple.py
@@ -4,7 +4,7 @@
 
 #自動打包，自動解包
 #應用1
-'''(one,two,three,four) = (1,2,3,4)
+(one,two,three,four) = (1,2,3,4)
 print(one)
 print(two)
 print(three)
@@ -14,15 +14,16 @@ one1,two1,three1,four1=1,2,3,4
 print(one1)
 print(two1)
 print(three1)
-print(four1)'''
+print(four1)
+
 #應用2
-'''a=3
+a=3
 b=4
 print(a,b)
 a,b=b,a
-print(a,b)'''
-
+print(a,b)
+print('\n')
 #list和tuple轉換
-'''print(list((1,2,3,4)))
+print(list((1,2,3,4)))
 print(tuple([1,2,3,4]))
-print(list("HELLO"))'''
+print(list("HELLO"))
